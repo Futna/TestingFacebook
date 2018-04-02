bc6f02c2f82ae69ad3e5720900a9f89c1ca67ec1
Futna shoqo <fshoqo@gmail.com>
2018-04-02 22:55:21 +0300

    first commit

---
+++ src/najah/edu/TestFacebook.java
@@ -0,0 +1,23 @@
+package najah.edu;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestFacebook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\futnashoqo\\Downloads\\geckodriver-v0.19.1-win64\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("ya_2lbi_f@hotmail.com");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("fatoon2332018");
+
+		
+		driver.findElement(By.xpath(".//*[@id='u_0_2']")).click();
+	}
+
+}
